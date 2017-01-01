9553df13bbbc1e507ff504924c1e379a04799cad
Vladislav Pazych <vladpazych@gmail.com>
2017-01-02 00:42:25 +0200

    Fix generate without watch

--- main.js
+++ main.js
@@ -2,28 +2,31 @@ var child_process = require('child_process');
 var path = require('path');
 var watch = require('node-watch');
 
-module.exports = function (watchDirOrMap, mapFilePath) {
+var generate = function (mapFilePath) {
+    var mainPath = path.dirname(require.main.filename);
+    var mapFileAbsolutePath = path.join(mainPath, mapFilePath);
+    runGenerateServer(mapFileAbsolutePath);
+}
+
+generate.watch = function (watchDir, mapFilePath) {
     var mainPath = path.dirname(require.main.filename);
     var mapFileAbsolutePath = path.join(mainPath, mapFilePath);
 
     runGenerateServer(mapFileAbsolutePath);
 
-    if (typeof watchDirOrMap == 'string') {
-        watch(watchDirOrMap, function () {
+    if (typeof watchDir == 'string') {
+        watch(watchDir, function () {
             runGenerateServer(mapFileAbsolutePath);
         });
     }
 }
 
+
+module.exports = generate;
+
 function runGenerateServer(mapFilePath) {
     var serverFilePath = path.join(__dirname, 'server.js');
     var command = "node " + serverFilePath + " " + mapFilePath;
 
-    // child_process.exec(command, function (err, stdout, stderr) {
-    //     if (err) console.log(err);
-    //     if (stderr) console.log(stderr);
-    //     console.log(stdout);
-    // });
-
-    console.log(child_process.execSync(command, {encoding: 'utf8'}));
+    console.log(child_process.execSync(command, { encoding: 'utf8' }));
 }
